917ca3c46590dc5606e1cb6d3f9eedd69c1dfff9
nikisha-script <nikisha_script@mail.ru>
2022-09-08 17:49:06 +0300

    1. Фильтры для объявлений машин [#4745]

---
+++ src/main/java/ru/job4j/cars/store/PostRepository.java
@@ -0,0 +1,36 @@
+package ru.job4j.cars.store;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import ru.job4j.cars.model.Post;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@AllArgsConstructor
+public class PostRepository implements Crud {
+
+    private final SessionFactory sf;
+
+    public List<Post> findAllPostFromLastDay() {
+        return query("from Post as p where created = now()", Post.class, sf);
+    }
+
+    public List<Post> findAllPostFromCarHasPhoto() {
+        return command(session ->
+                session.createQuery("select c from Post join fetch c.cars where c.photo != null").list(), sf);
+    }
+
+    public List<Post> findAllPostOfCertainBrand(String brand) {
+        return query(
+                "select c from Post join fetch c.cars where c.name like :fKey",
+                Post.class,
+                Map.of("fKey", brand),
+                sf
+        );
+    }
+
+
+}
